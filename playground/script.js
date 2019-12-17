2a9b1d8ab0685d28b8db11fb68f501be005ae1cc
Joby Joseph <reachjoby@gmail.com>
2019-12-17 11:50:13 +0530

    More on numbers

---
+++ playground/script.js
@@ -0,0 +1,13 @@
+// Boolean to Number
+console.log(true * 1);  // 1
+console.log(false * 1); // 0
+
+// String to Number
+console.log("123" * 1); // 123
+console.log("abc" * 1); // NaN
+
+// Null to Number
+console.log(null * 1);  // 0
+
+// Undefined to Number
+console.log(undefined * 1); // NaN
