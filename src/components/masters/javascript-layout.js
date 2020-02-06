18b759546e4800951d948b5ceca87321f4bfd14b
Joby Joseph <reachjoby@gmail.com>
2020-02-06 13:36:13 +0530

    setuptest issue post

--- src/components/masters/javascript-layout.js
+++ src/components/masters/javascript-layout.js
@@ -12,29 +12,8 @@ import PropTypes from "prop-types"
 const Layout = ({ children }) => {
   return (
     <div className="container">
-      <div className="grid-2">
-        <div className="col-1">
-          <div className="sidebarMenu">
-            <h3>JavaScript</h3>
-            <ul>
-              <li><Link to="/javascript/introduction">Introduction</Link></li>
-              <li><Link to="/javascript/variables">Variables</Link></li>
-              <li><Link to="/javascript/boolean-data-type">Boolean Data Type</Link></li>
-              <li><Link to="/javascript/number-data-type">Number Data Type</Link></li>
-              <li><Link to="/javascript/string-data-type">String Data Type</Link></li>
-              <li><Link to="/javascript/symbol-data-type">Symbol Data Type</Link></li>
-              <li><Link to="/javascript/spread-operator">Spread Operator</Link></li>
-              <li><Link to="/javascript/destructuring">Destructuring</Link></li>
-              <li><Link to="/javascript/objects">Objects</Link></li>
-              <li><Link to="/javascript/prototypes">Prototypes</Link></li>
-              <li><Link to="/javascript/callbacks">Callbacks</Link></li>
-              <li><Link to="/javascript/promises">Promises</Link></li>
-              <li><Link to="/javascript/es2020-new-features">ES2020 New Features</Link></li>
-              <li><Link to="/javascript/interview-questions-1">JavaScript Interview Questions</Link></li>
-            </ul>
-          </div>
-        </div>
-        <div className="col-2">{children}</div>
+      <div className="contentBox">
+        {children}
       </div>
     </div>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 import styles from "../styles/index.module.scss";
 
@@ -10,50 +9,55 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Learn JavaScript and Node.js" />
     <div className="container">
-      <h3 className={styles.sectionHeader}>JavaScript</h3>
-      <div className={styles.grid}>
-        <div className={styles.gridItem}>
-          1. <Link to="/javascript/introduction">Introduction</Link>
-        </div>
-        <div className={styles.gridItem}>
-          2. <Link to="/javascript/variables">Variables</Link>
-        </div>
-        <div className={styles.gridItem}>
-          3. <Link to="/javascript/boolean-data-type">Boolean Data Type</Link>
-        </div>
-        <div className={styles.gridItem}>
-          4. <Link to="/javascript/number-data-type">Number Data Type</Link>
-        </div>
-        <div className={styles.gridItem}>
-          5. <Link to="/javascript/string-data-type">String Data Type</Link>
-        </div>
-        <div className={styles.gridItem}>
-          6. <Link to="/javascript/symbol-data-type">Symbol Data Type</Link>
-        </div>
-        <div className={styles.gridItem}>
-          7. <Link to="/javascript/spread-operator">Spread Operator</Link>
-        </div>
-        <div className={styles.gridItem}>
-          8. <Link to="/javascript/destructuring">Destructuring</Link>
-        </div>
-        <div className={styles.gridItem}>
-          9. <Link to="/javascript/objects">Objects</Link>
-        </div>
-        <div className={styles.gridItem}>
-          10. <Link to="/javascript/prototypes">Prototypes</Link>
-        </div>
-        <div className={styles.gridItem}>
-          11. <Link to="/javascript/callbacks">Callbacks</Link>
-        </div>
-        <div className={styles.gridItem}>
-          12. <Link to="/javascript/promises">Promises</Link>
-        </div>
-        <div className={styles.gridItem}>
-          13. <Link to="/javascript/es2020-new-features">ES2020 New Features</Link>
-        </div>
-        <div className={styles.gridItem}>
-          14. <Link to="/javascript/interview-questions-1">JavaScript Interview Questions</Link>
-        </div>
+      <div className="contentBox">
+        <h1>Articles</h1>
+        <ol reversed className={styles.articleList}>
+          <li>
+            <Link to="/create-react-app-setuptests-js-not-working">SOLVED: setupTests.js not working in CreateReactApp</Link>
+          </li>
+          <li>
+            <Link to="/javascript/interview-questions-1">JavaScript Interview Questions</Link>
+          </li>
+          <li>
+            <Link to="/javascript/es2020-new-features">ES2020 New Features</Link>
+          </li>
+          <li>
+            <Link to="/javascript/promises">Promises in JavaScript</Link>
+          </li>
+          <li>
+            <Link to="/javascript/callbacks">Callbacks in JavaScript</Link>
+          </li>
+          <li>
+            <Link to="/javascript/prototypes">Prototypes in JavaScript</Link>
+          </li>
+          <li>
+            <Link to="/javascript/objects">Objects in JavaScript</Link>
+          </li>
+          <li>
+            <Link to="/javascript/destructuring">Destructuring in JavaScript</Link>
+          </li>
+          <li>
+            <Link to="/javascript/spread-operator">Spread Operator in JavaScript</Link>
+          </li>
+          <li>
+            <Link to="/javascript/symbol-data-type">Symbol Data Type in JavaScript</Link>
+          </li>
+          <li>
+            <Link to="/javascript/string-data-type">String Data Type in JavaScript</Link>
+          </li>
+          <li>
+            <Link to="/javascript/number-data-type">Number Data Type in JavaScript</Link>
+          </li>  
+          <li>
+            <Link to="/javascript/boolean-data-type">Boolean Data Type in JavaScript</Link>
+          </li>
+          <li>
+            <Link to="/javascript/variables">Variables in JavaScript</Link>
+          </li>
+          <li>
+            <Link to="/javascript/introduction">Introduction to JavaScript</Link>
+          </li>
+        </ol>
       </div>
     </div>
   </Layout>
