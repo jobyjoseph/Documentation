c26cab52bdf10bbdf62c04b2c0e8832ec9ec93d0
Joby Joseph <reachjoby@gmail.com>
2020-02-10 11:09:30 +0530

    Create react app code

---
+++ test/component.js
@@ -0,0 +1,2 @@
+const MyComponent = React.createElement("div", null, React.createElement("h1", null, "Blog Title"), React.createElement("p", null, "Here is the blog description."));
+ReactDOM.render(MyComponent, document.getElementById("root"));
