59bf00036651ce28cb740a48305997a93656cb13
Joby Joseph <reachjoby@gmail.com>
2021-02-21 11:34:49 +0530

    3rd daily

--- src/components/utils/Posts/index.js
+++ src/components/utils/Posts/index.js
@@ -7,6 +7,10 @@ function Posts(props) {
     <div className={styles.postsWrapper}>
       <ul>
         <li>
+          <Link to="/javascript/js-daily-3-swap-values-destructuring">JS Daily #3 - Swap Values using Destructuring</Link>{" "}
+          - Feb 20, 2021
+        </li>
+        <li>
           <Link to="/javascript/js-daily-2-arrow-function-this">JS Daily #2 - this in Arrow Function</Link>{" "}
           - Feb 19, 2021
         </li>
