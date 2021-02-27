2d8a1f486a5928f6afd99481bbc6553da055955e
Joby Joseph <reachjoby@gmail.com>
2021-02-27 22:21:25 +0530

    Removed Ad code

--- src/components/utils/Posts/index.js
+++ src/components/utils/Posts/index.js
@@ -1,12 +1,8 @@
-import React, {useEffect} from "react"
+import React from "react"
 import { Link } from "gatsby"
 import styles from "./style.module.scss"
 
 function Posts(props) {
-
-  useEffect(() => {
-    (window.adsbygoogle = window.adsbygoogle || []).push({});
-  }, []);
 
   return (
     <div className={styles.postsWrapper}>
