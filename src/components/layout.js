6dc8ab3afcc4cb88dc3f20e99535074f01d83884
Joby Joseph <reachjoby@gmail.com>
2021-04-05 23:49:30 +0530

    smaller css file

--- src/components/layout.js
+++ src/components/layout.js
@@ -18,7 +18,7 @@ const Layout = ({ children }) => {
   return (
     <>
       <Helmet>
-        <link rel="stylesheet" href="https://use.typekit.net/nxq8pto.css" />
+        <link rel="stylesheet" href="https://use.typekit.net/sut3opv.css" />
         <script src="https://chimpstatic.com/mcjs-connected/js/users/46e3529af04e73e3a37742ae8/787188458634130b24778b31b.js" async></script>
       </Helmet>
       <Header/>
