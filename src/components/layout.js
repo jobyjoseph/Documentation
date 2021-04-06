fa4c929b2d2d02f5cf4d57915d83542a6b97e996
Joby Joseph <reachjoby@gmail.com>
2021-04-06 23:41:34 +0530

    Removed chimp and layout ad

--- src/components/layout.js
+++ src/components/layout.js
@@ -19,12 +19,8 @@ const Layout = ({ children }) => {
     <>
       <Helmet>
         <link rel="stylesheet" href="https://use.typekit.net/sut3opv.css" />
-        <script src="https://chimpstatic.com/mcjs-connected/js/users/46e3529af04e73e3a37742ae8/787188458634130b24778b31b.js" async></script>
       </Helmet>
       <Header/>
-      <div className={styles.adContainer}>
-        <GoogleAds slot="9034656808" />
-      </div>
       <div>
         <main>{children}</main>
       </div>
